04d4929641cff0e15ccc3601e1de921f53442fb3
robstarbuck <robstarbuck@gmail.com>
2024-03-06 23:14:46 +0000

    Create demo pages using the .create-page class

--- _config.ts
+++ _config.ts
@@ -14,7 +14,7 @@ site.data("buildTime", time);
 site.data("github", "https://github.com/robstarbuck");
 site.data("logoFill", "#2b2031");
 
-site.copy([".pdf", ".png"]);
+site.copy([".pdf", ".png", ".svg"]);
 
 site.copy("code", ".");
 site.copy("styles", ".");
@@ -24,7 +24,11 @@ site.use(codeHighlight());
 
 // Site Process
 
-const wrapElement = (el: HTMLElement, wrapper: HTMLElement) => {
+const isCustomTag = (el?: Element | null): el is Element => {
+  return el?.tagName.includes("-") ?? false;
+};
+
+const wrapElement = (el: Element, wrapper: HTMLElement) => {
   if (el.parentNode) {
     el.parentNode.insertBefore(wrapper, el);
     wrapper.appendChild(el);
@@ -34,6 +38,10 @@ const wrapElement = (el: HTMLElement, wrapper: HTMLElement) => {
 // test for tagName, instanceof is unavailable in this context
 const isIframe = (e: Element): e is HTMLIFrameElement => {
   return e.tagName === "IFRAME";
+};
+
+const isSVG = (e: Element): e is SVGElement => {
+  return e.tagName === "SVG";
 };
 
 const isAnchor = (e: Element): e is HTMLAnchorElement => {
@@ -52,7 +60,9 @@ site.preprocess([".html"], (pages) => {
   pages.forEach((page) => {
     if (/posts/.test(page.src.path)) {
       page.data.tags = ["post"];
-      page.data.layout = "index.vto";
+      if (page.data.layout === undefined) {
+        page.data.layout = "index.vto";
+      }
     }
     if (page.data.title?.toLocaleLowerCase() === "cv") {
       const [cvDate] = page.data.date.toISOString().split("T");
@@ -84,24 +94,71 @@ site.process([".html"], (pages, allPages) => {
     if (document === undefined) {
       return;
     }
-    document.querySelectorAll("iframe, a, img").forEach((element) => {
-      if (isIframe(element)) {
-        const div = document.createElement("div");
-        div.classList.add("iframe");
-        wrapElement(element, div);
-      }
-      if (isAnchor(element)) {
-        const href = element.getAttribute("href");
-        if (href && isLinkExternal(href)) {
-          element.setAttribute("target", "_blank");
+
+    // Create demo pages wherever a .create-page element exists
+    document
+      .querySelectorAll("main :where(.create-page)")
+      .forEach((element) => {
+        // Avoid mutating the original page
+        const doc = page.document?.cloneNode(true) as Document;
+        const main = doc?.querySelector("main");
+        const nav = doc?.querySelector("nav");
+        const footer = doc?.querySelector("footer");
+        if(nav){
+          nav.parentElement?.removeChild(nav)
         }
-      }
-      if (isImage(element)) {
-        const div = document.createElement("div");
-        div.classList.add("image");
-        wrapElement(element, div);
-      }
-    });
+        if (footer) {
+          footer.parentElement?.removeChild(footer);
+        }
+        if (main) {
+          main.outerHTML = element.outerHTML;
+        }
+
+        const demoPage = Page.create({
+          title: page.data.title,
+          url: page.data.url + "demo",
+          content: doc?.documentElement.outerHTML,
+          layout: "index.vto",
+        });
+        allPages.push(demoPage);
+      });
+
+    document
+      .querySelectorAll("main :where(iframe, a, img, svg)")
+      .forEach((element) => {
+        if (isCustomTag(element.parentElement)) {
+          const div = document.createElement("div");
+          div.classList.add("custom");
+          wrapElement(element.parentElement, div);
+          return;
+        }
+        if (isIframe(element)) {
+          const div = document.createElement("div");
+          div.classList.add("iframe");
+          wrapElement(element, div);
+          return;
+        }
+        if (isSVG(element)) {
+          const div = document.createElement("div");
+          div.classList.add("svg");
+          wrapElement(element, div);
+          return;
+        }
+        if (isAnchor(element)) {
+          const href = element.getAttribute("href");
+          if (href && isLinkExternal(href)) {
+            element.setAttribute("target", "_blank");
+            return;
+          }
+          return;
+        }
+        if (isImage(element)) {
+          const div = document.createElement("div");
+          div.classList.add("image");
+          wrapElement(element, div);
+          return;
+        }
+      });
   });
 
   const readMe = Page.create({
